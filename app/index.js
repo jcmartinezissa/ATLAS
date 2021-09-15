d84403f11ad5ffbd6deb578c041fbf1b1fa595b2
Ignacio <ignaciopaliza@gmail.com>
2021-09-15 20:14:11 -0300

    algunos cambios

---
+++ app/index.js
@@ -0,0 +1,28 @@
+const cuentas= [];
+class Cuenta {
+
+    constructor(id,nombre,clave,admin){
+        this.id = id;
+        this.nombre = nombre;
+        this.clave = clave;
+        this.admin = admin;
+    }
+    
+}
+
+function Altas(id,nombre,clave,admin){
+
+    //traer el array de usuarios 
+    localStorage.setItem("llave","valor");
+        cuentas = localStorage.getItem('Cuenta');
+
+
+     for(){
+
+     }
+    window.localStorage.getItem(nombre)//recuperar
+    ()
+    localStorage.setItem("") //crear
+
+}
+
