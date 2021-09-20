756e61b3aa1d31273c6c9a7b2206cdf2427a0742
JUAN CARLOS MARTINEZ ISSA <carloswolvi@gmail.com>
2021-09-20 20:08:12 -0300

    login 50%

--- app/index.js
+++ app/index.js
@@ -1,28 +1,71 @@
-const cuentas= [];
+const cuentas = [];
 class Cuenta {
+  constructor(nombre, clave, correo, avatar, admin) {
+    this.nombre = nombre;
+    this.clave = clave;
+    this.correo = correo;
+    this.avatar = avatar;
+    this.admin = admin;
+  }
+}
 
-    constructor(id,nombre,clave,admin){
-        this.id = id;
-        this.nombre = nombre;
-        this.clave = clave;
-        this.admin = admin;
+function leerUsuarios() {
+  if (localStorage.getItem("Cuentas")) {
+    cuentas = JSON.parse(localStorage.getItem("Cuentas"));
+  }
+}
+
+function NuevoUsuario() {
+  let usuario = document.getElementById("usuario1").value;
+  let clave = document.getElementById("contrasenia1").value;
+  let avatar = document.getElementById("avatar").value;
+  let correo = document.getElementById("correo").value;
+
+  let nuevo = new Cuenta(usuario, clave, avatar, correo);
+
+  leerUsuarios();
+
+  cuentas.forEach(function Buscar(e) {
+    if (cuentas.nombre == usuario) {
+      alert("el usuario ya existe");
+      return;
     }
-    
+  });
+  cuentas.push(nuevo);
+  localStorage.setItem("cuentas", JSON.stringify(nuevo));
+  alert("Agregado");
+  document.getElementById("CerrarRegistro").click();
 }
 
-function Altas(id,nombre,clave,admin){
+function LogIn() {
 
-    //traer el array de usuarios 
-    localStorage.setItem("llave","valor");
-        cuentas = localStorage.getItem('Cuenta');
+    let usuario = document.getElementById("usuario").value;
+    let clave = document.getElementById("contrasenia").value;
+    let entro = false;
 
+  leerUsuarios();
 
-     for(){
-
-     }
-    window.localStorage.getItem(nombre)//recuperar
-    ()
-    localStorage.setItem("") //crear
-
+  cuentas.forEach(function validar(e) {
+    if (cuentas.nombre == usuario && cuentas.clave == clave) {
+      entro = true;
+    }
+  });
+  if ((entro = true)) {
+    document.getElementById("btninicio").hidden;
+    document.getElementById("btnRegistro").hidden = true;
+    document.getElementById("btnFavoritos").hidden = false;
+  } else {
+    alert("Nombre de usuario o contraseña no validos");
+  }
 }
+
+function EliminarUsuario(orden){
+
+    listadeusuarios.splice(inx, 1);
+    localStorage.setItem("usuarios", JSON.stringify(listadeusuarios));
+    console.log("usuario:" + usuario + " eliminado");
+    break;
+
+
+};
 
